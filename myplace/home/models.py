0bd86da4d801eba947b30d2a6c5ec39e63e14eca
Begen Yussupov <devsteppe@gmail.com>
2020-01-10 03:51:18 +0600

    added header and footer, chaned base dir

--- myplace/home/models.py
+++ myplace/home/models.py
@@ -6,3 +6,7 @@ class Place(models.Model):
     place_phone=models.IntegerField()
     place_address=models.CharField(max_length=255)
     rating=models.PositiveIntegerField()
+    #place_img=models.ImageField()
+
+    def __str__(self):
+        return 'Name {} # {} {} Rating: {}'.format(self.place_name, self.place_phone, self.place_address, self.place_address)
--- myplace/home/views.py
+++ myplace/home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-
+from .models import Place
 # Create your views here.
 
 def home_page(request):
-    return  render(request,'index.html',{})
+    ctx={}
+    all_places=Place.objects.all()
+    for place in all_places:
+        print(place.place_name)
+    ctx['places']=all_places
+    return  render(request,'home/home_page.html',ctx)
