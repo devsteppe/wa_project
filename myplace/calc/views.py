9a0a1e00b31bdf93dee3155656201e9f85f470ca
Begen Yussupov <devsteppe@gmail.com>
2020-01-12 09:00:30 +0600

    added calc

--- myplace/calc/views.py
+++ myplace/calc/views.py
@@ -3,6 +3,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 def calc(request):
-    ctx={}
-    ctx['tab']='calc'
-    return render(request,'calc/calc_form.html',ctx)
+    from .calc_obj import obj as CALC
+    ctx = {}
+    ctx['operation']=CALC.keys()
+    ctx['tab'] = 'calc'
+    template_name='calc/calc_form.html'
+    if request.method=='GET':
+
+        return render(request,template_name,ctx)
+
+    elif request.method=='POST':
+        try:
+            first_number=float(request.POST.get('first'))
+            operation=request.POST.get('operation')
+            second_number=float(request.POST.get('second'))
+            res=CALC[operation](first_number,second_number)
+            ctx['result']=res
+        except Exception as e:
+            print(e)
+            ctx['errors']=e
+            return render(request,template_name,ctx)
+        return render(request,template_name,ctx)
